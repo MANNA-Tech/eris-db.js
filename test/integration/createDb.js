5f2e6a57b226995bc16e60f13c3faf5c5d00d081
David Braun <David.Braun@Toptal.com>
2015-10-19 18:35:47 -0400

    Clean test blockchain more thoroughly before each integration test.

--- test/integration/createDb.js
+++ test/integration/createDb.js
@@ -1,16 +1,17 @@
 'use strict';
 
 var
-  child_process = require('child_process')
+  child_process = require('child_process'),
   Promise = require('bluebird');
 
 Promise.promisifyAll(child_process);
 
-// Create a fresh chain for each integration test and return its IP address.
+// Create a fresh chain for each integration test.  Return its IP address and
+// private key.
 module.exports = function () {
   return child_process.execAsync('\
     eris chains stop blockchain; \
-    eris chains rm blockchain; \
+    eris chains rm --data blockchain; \
     eris chains new --genesis=blockchain/genesis.json \
       --priv=blockchain/priv_validator.json --api --publish blockchain \
     && eris chains start blockchain \
@@ -20,7 +21,7 @@ module.exports = function () {
       var
         ipAddress, privateKey;
 
-      ipAddress = /.*\n(.*)\n/.exec(stdout)[1];
+      ipAddress = /(\d+\.\d+\.\d+\.\d+)\n/.exec(stdout)[1];
       console.log("Created ErisDB test server at " + ipAddress + ".");
       privateKey = require('./blockchain/priv_validator.json').priv_key[1];
       return [ipAddress, privateKey];
